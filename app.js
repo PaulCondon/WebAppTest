5988b887d517e84859e551be60dfbd90275a6e83
PaulCondon <paul.condon96@gmail.com>
2019-02-16 15:46:44 +0000

    Galway Marker

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 const http = require('http');
 const fs = require('fs');
-
+const port = '3000';
 fs.readFile('index.html', (err, html) =>{
     if(err){
         throw err;
     }
 
-    const server = http.createServer((req ,res) => {
+    http.createServer((req ,res) => {
         res.statusCode = 200;
         res.setHeader('Content-type', 'text/html');
         res.write(html);
         res.end();
-    });
-    
-    server.listen(port, hostname, () => {
-        console.log('Server started n port ' + port);
-    });
+    }).listen(port);
+     console.log('Listening on port', port);
 
 });
 
-const hostname = '127.0.0.1';
-const port = '3000';
 
 
 
-//
-//
+
+// var http = require('http');
+
+// var port = 3000;
+// http.createServer(function (req, res) {
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end('Hello there, world!\n');
+
+// }).listen(port);
+// console.log('Listening on port', port);
+
+
