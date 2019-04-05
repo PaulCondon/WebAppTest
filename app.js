ca4fc130eb63d68cde2e6c5ce2e069206fd603f1
PaulCondon <paul.condon96@gmail.com>
2019-04-05 11:13:05 +0100

    Refinement and deletd old code snippets

--- app.js
+++ app.js
@@ -16,34 +16,3 @@ fs.readFile('index.html', (err, html) =>{
      console.log('Listening on port', port);
 
 });
-
-// var express = require('express')
-// var fs = require('fs')
-// var https = require('https')
-// var app = express()
-
-// app.get('/', function (req, res) {
-//   res.send('hello world')
-// })
-
-// https.createServer({
-//   key: fs.readFileSync('server.key'),
-//   cert: fs.readFileSync('server.cert')
-// }, app)
-// .listen(3000, function () {
-//   console.log('Example app listening on port 3000! Go to https://localhost:3000/')
-// })
-
-
-
-// var http = require('http');
-
-// var port = 3000;
-// http.createServer(function (req, res) {
-//         res.writeHead(200, {'Content-Type': 'text/plain'});
-//         res.end('Hello there, world!\n');
-
-// }).listen(port);
-// console.log('Listening on port', port);
-
-
